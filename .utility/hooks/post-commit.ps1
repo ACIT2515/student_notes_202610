6c3bcb48afc4b320fd4ab7bb683f56f897d9cd0f
Thomas Lane <thomas_lane@bcit.ca>
2026-01-07 20:24:18 -0800

    updated references

--- .utility/hooks/post-commit.ps1
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env pwsh
-# hooks are run with project root as cwd
-python .\.git\hooks\restore_links.py ".\.sym_links.json"
-git add .\.sym_links.json
--- .utility/hooks/pre-commit.ps1
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env pwsh
-# hooks are run with project root as cwd
-python .\.git\hooks\process_links.py ".\" ".\.sym_links.json"
